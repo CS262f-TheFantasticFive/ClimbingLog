bddd701a4c685b442a079f19822d0f267faba64f
Chris <codemonkey941@gmail.com>
2015-11-04 12:29:03 -0500

    Pushing up my classes and edited work.

--- app/src/main/java/edu/calvin/cs262/climbinglogapp/Profile.java
+++ app/src/main/java/edu/calvin/cs262/climbinglogapp/Profile.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 
 /**
  * Created by cpd5 on 10/12/2015.
+ * This is the page that will contain the profile of the user.
  */
 public class Profile extends Activity {
 
-
+    //onCreate() method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
