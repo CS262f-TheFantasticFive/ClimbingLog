bab59122de692490b0c56165d5a8aa4cba6ad317
abs25 <abs25@students.calvin.edu>
2015-11-04 15:40:47 -0500

    Added nav bar fragment, now it's on every page. Also fixed green button so it's smaller.

--- app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
+++ app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
@@ -1,6 +1,6 @@
 package edu.calvin.cs262.climbinglogapp;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,5 +14,6 @@ public class NavBar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.navbar_view, container, false);
+
     }
 }
