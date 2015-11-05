1e8899ec8a1d6ed66b0b25db1e9ad69f89ca872a
Chris <codemonkey941@gmail.com>
2015-11-05 16:21:18 -0500

    Added onClickListeners for each NavBar button :D I still have to find a way to disable the corresponding button on the NavBar when you are taken to the page (to prevent someone from creating the same Activity repeatedly).

--- app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
+++ app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
@@ -16,16 +16,24 @@ import android.widget.Button;
 public class NavBar extends Fragment {
 
     /**
+     * This is the method that creates the NavBar.
+     * onClickListeners() are placed on each button so that when you click a button on the NavBar,
+     * You are taken to that page. 
      * Adapted from StackOverflow... http://stackoverflow.com/questions/8977212/button-click-listeners-in-android
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return
+     *
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Create the NavBar view
         View NavBarView = inflater.inflate(R.layout.navbar_view, container, false);
-        Button profile = (Button) NavBarView.findViewById(R.id.button3);
+        //Get the buttons
+        Button profile = (Button) NavBarView.findViewById(R.id.profile_button);
+        Button gyms = (Button) NavBarView.findViewById(R.id.gyms_button);
+        Button friends = (Button) NavBarView.findViewById(R.id.friends_button);
+        Button routes = (Button) NavBarView.findViewById(R.id.routes_button);
+
+        //Set on click listeners for each button
+        //Profile
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,7 +42,32 @@ public class NavBar extends Fragment {
             }
         });
 
+        //Gyms
+        gyms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gymIntent = new Intent(getActivity(), Gyms.class);
+                startActivity(gymIntent);
+            }
+        });
 
+        //Friends
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent friendIntent = new Intent(getActivity(), Friends.class);
+                startActivity(friendIntent);
+            }
+        });
+
+        //Routes
+        routes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent routeIntent = new Intent(getActivity(), Routes.class);
+                startActivity(routeIntent);
+            }
+        });
         return NavBarView;
     }
 
