a4326a4894cc2734bbcb396c56651749b46f772e
Kirby <codemonkey941@gmail.com>
2015-11-05 11:17:14 -0500

    Trying to merge...

--- app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
+++ app/src/main/java/edu/calvin/cs262/climbinglogapp/NavBar.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 /**
  * Created by Chris on 11/2/2015.
@@ -24,37 +22,8 @@ public class NavBar extends Fragment {
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View NavBarView = inflater.inflate(R.layout.navbar_view, container, false);
-        Button profile = (Button) NavBarView.findViewById(R.id.button3);
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent profileIntent = new Intent(getActivity(), Profile.class);
-                startActivity(profileIntent);
-            }
-        });
+        return inflater.inflate(R.layout.navbar_view, container, false);
 
-
-        return NavBarView;
     }
 
-    public void startProfile(View view) {
-        Intent profileIntent = new Intent(getActivity(), Profile.class);
-        startActivity(profileIntent);
-    }
-
-//    public void startGym(View view) {
-////        Intent gymIntent = new Intent(getActivity(), Profile.class);
-////        startActivity(gymIntent);
-//    }
-//
-//    public void startFriends(View view) {
-//        Intent profileIntent = new Intent(getActivity(), Profile.class);
-//        startActivity(profileIntent);
-//    }
-//
-//    public void startRoutes(View view){
-//        Intent profileIntent = new Intent(getActivity(), Profile.class);
-//        startActivity(profileIntent);
-//    }
 }
